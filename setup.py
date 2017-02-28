b0b0a6bff22be3ec3dbd3acb2ed0bf5397aa4973
Ismael <ismael@atlasense.com>
2017-02-28 15:10:10 -0500

    Package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='elasticlogic',
+    version='0.1',                          # Update the version number for new releases
+    url='https://github.com/gitcarbs/elasticlogic',
+    install_requires = [
+        'python-dateutil'
+    ]
+)
